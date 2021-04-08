d56197a467e47ba7b644a1b90518238becba09b4
Jonathan Snider <jtsnider0713@gmail.com>
2021-04-08 15:28:25 -0500

    Update its_just_us.py
    Updated to Python naming conventions

--- its_just_us/its_just_us.py
+++ its_just_us/its_just_us.py
@@ -22,21 +22,21 @@ for cog in extensions:
     print("There was an error loading {0}: {1}".format(cog, e))
 
 
-async def getMessageText(msgId):
+async def get_message_text(msg_id):
   conn = sqlite3.connect('dbs/schedules.db')
   c = conn.cursor() 
 
   sql = "SELECT schedule_text FROM schedules WHERE schedule_id = ?"
-  c.execute(sql,(msgId,))
-  msgTxt = c.fetchone()
+  c.execute(sql,(msg_id,))
+  msg_txt = c.fetchone()
 
   c.close()
   conn.close()
-  if(msgTxt != None):
-    return msgTxt[0]
-  return msgTxt
+  if(msg_txt != None):
+    return msg_txt[0]
+  return msg_txt
 
-async def updatePostedSchedules(msgId, scheduleId):
+async def update_posted_schedules(msg_id, schedule_id):
   conn = sqlite3.connect('dbs/posted_schedules.db')
   c = conn.cursor() 
 
@@ -45,21 +45,21 @@ async def updatePostedSchedules(msgId, scheduleId):
   conn.commit()
   
   sql = "INSERT INTO posted_schedules VALUES(?,?)"
-  c.execute(sql, (msgId, scheduleId))
+  c.execute(sql, (msg_id, schedule_id))
   conn.commit()
 
   c.close()
   conn.close()
 
-async def getPostedScheduleMessage(scheduleId):
+async def get_posted_schedule_message(schedule_id):
   conn = sqlite3.connect('dbs/posted_schedules.db')
   c = conn.cursor() 
 
   sql = "SELECT message_id FROM posted_schedules"
   c.execute(sql)
-  postedScheduleMessage = c.fetchone()
-  if(postedScheduleMessage != None):
-    postedScheduleMessage = postedScheduleMessage[0]
+  posted_schedule_message = c.fetchone()
+  if(posted_schedule_message != None):
+    posted_schedule_message = posted_schedule_message[0]
   else:
     c.close()
     conn.close()
@@ -67,17 +67,17 @@ async def getPostedScheduleMessage(scheduleId):
 
   c.close()
   conn.close()
-  return postedScheduleMessage
+  return posted_schedule_message
 
-async def getCurrentScheduleMessage(scheduleId):
+async def get_current_schedule_message(schedule_id):
   conn = sqlite3.connect('dbs/schedules.db')
   c = conn.cursor() 
 
   sql = "SELECT message_id FROM schedules"
   c.execute(sql)
-  currentScheduleMessage = c.fetchone()
-  if(currentScheduleMessage != None):
-    currentScheduleMessage = currentScheduleMessage[0]
+  current_schedule_message = c.fetchone()
+  if(current_schedule_message != None):
+    current_schedule_message = current_schedule_message[0]
   else:
     c.close()
     conn.close()
@@ -85,10 +85,10 @@ async def getCurrentScheduleMessage(scheduleId):
 
   c.close()
   conn.close()
-  return currentScheduleMessage
+  return current_schedule_message
 
 @tasks.loop(seconds=60.0)
-async def autoPostSchedule():
+async def auto_post_schedule():
   current_time = strftime("%H:%M", gmtime())
   current_day = strftime("%A", time.localtime())
   if(current_time == "06:00") and (current_day == "Sunday"):
@@ -99,35 +99,35 @@ async def autoPostSchedule():
     c.execute(sql)
     first_schedule = c.fetchall()[0]
     
-    channelId = 625399512608800796 # test channel 724077902521696395 weekly-schedule 625399512608800796
-    scheduleChannelId = 717868492313067560 # test channel 724077902521696395 weekly-sign-up 717868492313067560
-    scheduleId = first_schedule[1]
-    msgTxt = await getMessageText(scheduleId)
-    if(msgTxt != None):
-      msgTxt = msgTxt.split('\n\n\n\n')
-      del msgTxt[-1]
-      channelToSendTo = client.get_channel(channelId)
-      currentChannel = client.get_channel(scheduleChannelId)
+    channel_id = 625399512608800796 # test channel 724077902521696395 weekly-schedule 625399512608800796
+    schedule_channel_id = 717868492313067560 # test channel 724077902521696395 weekly-sign-up 717868492313067560
+    schedule_id = first_schedule[1]
+    msg_txt = await get_message_text(schedule_id)
+    if(msg_txt != None):
+      msg_txt = msg_txt.split('\n\n\n\n')
+      del msg_txt[-1]
+      channel_to_send_to = client.get_channel(channel_id)
+      current_channel = client.get_channel(schedule_channel_id)
       try:
-        oldMsgId = await getPostedScheduleMessage(scheduleId)
-        oldMsg = await channelToSendTo.fetch_message(int(oldMsgId))
-        await oldMsg.delete()
+        old_msg_id = await get_posted_schedule_message(schedule_id)
+        old_msg = await channel_to_send_to.fetch_message(int(old_msg_id))
+        await old_msg.delete()
 
-        oldScheduleMessageId = await getCurrentScheduleMessage(scheduleId)
-        oldScheduleMessage = await currentChannel.fetch_message(int(oldScheduleMessageId))
-        await oldScheduleMessage.delete()
+        old_schedule_message_id = await get_current_schedule_message(schedule_id)
+        old_schedule_message = await current_channel.fetch_message(int(old_schedule_message_id))
+        await old_schedule_message.delete()
       except Exception as e:
         print(e)
-        await channelToSendTo.send("There was an error deleting the old schedule.")
+        await channel_to_send_to.send("There was an error deleting the old schedule.")
       try:
-        newMsg = await channelToSendTo.send(f"New Schedule! @everyone\n\n{msgTxt[0]}")
-        await updatePostedSchedules(str(newMsg.id), scheduleId)
-        c.execute("DELETE FROM schedules WHERE schedule_id = ?", (scheduleId,))
+        new_msg = await channel_to_send_to.send(f"New Schedule! @everyone\n\n{msg_txt[0]}")
+        await update_posted_schedules(str(new_msg.id), schedule_id)
+        c.execute("DELETE FROM schedules WHERE schedule_id = ?", (schedule_id,))
       except Exception as e:
         print(e)
-        await channelToSendTo.send("There was an error posting the new schedule.")
+        await channel_to_send_to.send("There was an error posting the new schedule.")
     else:
-      await channelToSendTo.author.send("Not a valid ID")
+      await channel_to_send_to.author.send("Not a valid ID")
 
     conn.commit()
     c.close()
@@ -136,12 +136,12 @@ async def autoPostSchedule():
 @client.event
 async def on_ready():
   print("Ready!")
-  if(not autoPostSchedule.is_running()):
+  if(not auto_post_schedule.is_running()):
     print("Starting Auto Schedule Posting")
-    autoPostSchedule.start()
+    auto_post_schedule.start()
 
 # Run the bot
 with open('tokens.json', 'r') as f:
-  jsonInfoData = json.load(f)
-TOKEN = jsonInfoData["bot"]
+  tokens = json.load(f)
+TOKEN = tokens["bot"]
 client.run(TOKEN)
